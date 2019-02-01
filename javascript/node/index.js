4e8cb52e50c35e90045809be0efc380ce1a3f76c
work_2 <roger.angel@163.com>
2019-02-01 16:09:46 +0800

    node学习中

--- javascript/node/index.js
+++ javascript/node/index.js
@@ -1,20 +1,53 @@
-// 引入http模块,fs模块
+// 引入模块
 const http = require('http');
 const fs = require('fs');
-// 创建服务器对象
+/* http模块
+ *  http.createServer() //创建服务器对象
+ * 请求(req)相关方法
+ *  req.url // 域名后面字符串
+ * 响应(res)相关信息
+ *  res.setHead(name(string), value(any)) //设置响应头单个参数
+ *  res.writeHead(statusCode(number), statusMessage(string), headers(object))  //设置响应头信息
+ *  res.write(chunk(string|buffer), encoding(string[DEFAULT:utf8]), callback(function), returns(boolean)) //设置响应信息
+ *  res.end(chunk(string|buffer), encoding(string[DEFAULT:utf8]), callback(function), returns(boolean)) //设置响应信息
+ */
 const server = http.createServer(function (req, res) {
-    // fs调用readFile方法读取文件，非堵塞模式
-    fs.readFile('./index.html', function(err, data){
-        // 设置响应头信息
+    /*fs.readFile('./index.html', function(err, data){
         res.writeHead(200, {'Content-Type':'text/html; CHARSET=UTF-8'});
-        // 设置响应正文
-        res.end(data);
-    });
+    });*/
+
+    /* URL模块
+     *  Constructor: new URL(input[, base]) // url构造函数
+     *  url.hash     //锚点
+     *  url.host     //服务器地址
+     *  url.hostname //域名
+     *  url.href     //全链接
+     *  url.origin   //协议+域名
+     *  url.password //密码
+     *  url.port     //服务器端口
+     *  url.pathname //路径
+     *  url.protocol //协议
+     *  url.search   //查询字符串
+     */
+    const $url = new URL('http://127.0.0.1:3000/user/login?user=roger&pwd=123456ff#abc');
+    res.write($url.hash + '\n'); // #abc
+    res.write($url.host + '\n'); // 127.0.0.1:3000
+    res.write($url.hostname + '\n'); // 127.0.0.1
+    res.write($url.href + '\n');     // http://127.0.0.1:3000/user/login?user=roger&pwd=123456ff
+    res.write($url.origin + '\n');   // http://127.0.0.1:3000
+    res.write($url.password + '\n');
+    res.write($url.port + '\n');     // 3000
+    res.write($url.pathname + '\n'); // /user/login
+    res.write($url.protocol + '\n'); // http:
+    res.write($url.search + '\n');   // user=roger&pwd=123456ff
+
+    res.end();
 });
 // 监听3000端口
 server.listen(3000);
 
 console.log('nodeServer starting...');
+
 
 /*==================================================================================模块示例
  ./node-server.js ./class.js ./teacher.js ./student.js
