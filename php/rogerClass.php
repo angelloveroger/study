609a66a415923cc7e0e17c87003d5e7e193a0662
roger <roger.angel@163.com>
2019-08-28 16:57:47 +0800

    整理

--- php/rogerClass.php
+++ php/rogerClass.php
@@ -170,26 +170,6 @@ class rogerClass {
 
 
     //===================================================================获取文件后缀名的N+1种方法======================================================================
-
-    /**获取文件路径，文件名，文件后缀名
-     * @param $file 路径
-     * @return array
-     */
-    public function getPathInfo($file) {
-        $pathinfo = array( 'dirname' => pathinfo( $file, PATHINFO_DIRNAME ), 'basename' => pathinfo( $file, PATHINFO_BASENAME ), 'extension' => pathinfo( $file, PATHINFO_EXTENSION ), );
-        return $pathinfo;
-    }
-
-    /**
-     * @param $string
-     * @return string
-     */
-    public function getExt1($string) {
-        $pos = strripos( $string, '.' );
-        $ext = substr( $string, $pos + 1 );
-        return $ext;
-    }
-
     /**
      * strripos/strrpos(string,find [,start])  函数查找字符串在另一字符串中最后一次出现的位置,函数对大小写不敏感/函数对大小写敏感，返回字符串在另一字符串中最后一次出现的位置，如果没有找到字符串则返回 FALSE。
      * string 被搜索字符串   find 查找字符串  start 开始搜索的位置
@@ -212,6 +192,26 @@ class rogerClass {
      * strrev(string) 函数反转字符串，返回已反转的字符串
      * string 字符串
      */
+
+    /**获取文件路径，文件名，文件后缀名
+     * @param $file 路径
+     * @return array
+     */
+    public function getPathInfo($file) {
+        $pathinfo = array( 'dirname' => pathinfo( $file, PATHINFO_DIRNAME ), 'basename' => pathinfo( $file, PATHINFO_BASENAME ), 'extension' => pathinfo( $file, PATHINFO_EXTENSION ), );
+        return $pathinfo;
+    }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    public function getExt1($string) {
+        $pos = strripos( $string, '.' );
+        $ext = substr( $string, $pos + 1 );
+        return $ext;
+    }
+
 
     /**
      * @param $string
@@ -470,98 +470,42 @@ class rogerClass {
         }
     }
 
+    /**
+     * 通过CURL发送HTTP请求
+     * @param string $url  //请求URL
+     * @param array $postFields //请求参数
+     * @return mixed
+     */
+    function curlPost($url,$postFields){
+        extension_loaded('curl') ? NULL : EXIT('CURL NOT EXISTS!');
+        $postFields = http_build_query($postFields);
+        $ch = curl_init ();
+        curl_setopt ( $ch, CURLOPT_POST, 1 );
+        curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt ( $ch, CURLOPT_URL, $url );
+        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $postFields );
+        $result = curl_exec ( $ch );
+        if($result === false)
+        {
+            echo 'Curl error: ' . curl_error($ch) . '  ' . curl_errno($ch);;
+        }
+        curl_close ( $ch );
+        return $result;
+    }
+
+    /**生成随机名字
+     * @return string
+     */
+    function returnName(){
+        $familyName = '赵钱孙李周吴郑王冯陈褚卫蒋沈韩杨朱秦尤许何吕施张孔曹严华金魏陶姜戚谢邹喻柏水窦章云苏潘葛奚范彭郎鲁韦昌马苗凤花方俞任袁柳酆鲍史唐费廉岑薛雷贺倪汤滕殷罗毕郝邬安常乐于时傅皮卞齐康伍余元卜顾孟平黄和穆萧尹姚邵湛汪祁毛禹狄米贝明臧计伏成戴谈宋茅庞熊纪舒屈项祝董粱杜阮蓝闵席季麻强贾路娄危江童颜郭梅盛林刁钟徐邱骆高夏蔡田樊胡凌霍虞万支柯昝管卢莫经房裘缪干解应宗丁宣贲邓郁单杭洪包诸左石崔吉钮龚程嵇邢滑裴陆荣翁荀羊於惠甄麴家封芮羿储靳汲邴糜松井段富巫乌焦巴弓牧隗山谷车侯宓蓬全郗班仰秋仲伊宫宁仇栾暴甘钭厉戎祖武符刘景詹束龙叶幸司韶郜黎蓟薄印宿白怀蒲邰从鄂索咸籍赖卓蔺屠蒙池乔阴欎胥能苍双闻莘党翟谭贡劳逄姬申扶堵冉宰郦雍舄璩桑桂濮牛寿通边扈燕冀郏浦尚农温别庄晏柴瞿阎充慕连茹习宦艾鱼容向古易慎戈廖庾终暨居衡步都耿满弘匡国文寇广禄阙东殴殳沃利蔚越夔隆师巩厍聂晁勾敖融冷訾辛阚那简饶空曾毋沙乜养鞠须丰巢关蒯相查後荆红游竺权逯盖益桓公万俟司马上官欧阳夏侯诸葛闻人东方赫连皇甫尉迟公羊澹台公冶宗政濮阳淳于单于太叔申屠公孙仲孙轩辕令狐钟离宇文长孙慕容鲜于闾丘司徒司空亓官司寇仉督子车颛孙端木巫马公西漆雕乐正壤驷公良拓跋夹谷宰父谷梁晋楚闫法汝鄢涂钦段干百里东郭南门呼延归海羊舌微生岳帅缑亢况后有琴梁丘左丘东门西门商牟佘佴伯赏南宫墨哈谯笪年爱阳佟';
+        $secondName = ['澄邈','德泽','海超','海阳','海荣','海逸','海昌','瀚钰','瀚文','涵亮','涵煦','明宇','涵衍','浩皛','浩波','浩博','浩初','浩宕','浩歌','浩广','浩邈','浩气','浩思','浩言','鸿宝','鸿波','鸿博','鸿才','鸿畅','鸿畴','鸿达','鸿德','鸿飞','鸿风','鸿福','鸿光','鸿晖','鸿朗','鸿文','鸿轩','鸿煊','鸿骞','鸿远','鸿云','鸿哲','鸿祯','鸿志','鸿卓','嘉澍','光济','澎湃','彭泽','鹏池','鹏海','浦和','浦泽','瑞渊','越泽','博耘','德运','辰宇','辰皓','辰钊','辰铭','辰锟','辰阳','辰韦','辰良','辰沛','晨轩','晨涛','晨濡','晨潍','鸿振','吉星','铭晨','起运','运凡','运凯','运鹏','运浩','运诚','运良','运鸿','运锋','运盛','运升','运杰','运珧','运骏','运凯','运乾','维运','运晟','运莱','运华','耘豪','星爵','星腾','星睿','星泽','星鹏','星然','震轩','震博','康震','震博','振强','振博','振华','振锐','振凯','振海','振国','振平','昂然','昂雄','昂杰','昂熙','昌勋','昌盛','昌淼','昌茂','昌黎','昌燎','昌翰','晨朗','德明','德昌','德曜','范明','飞昂','高旻','晗日','昊然','昊天','昊苍','昊英','昊宇','昊嘉','昊明','昊伟','昊硕','昊磊','昊东','鸿晖','鸿朗','华晖','金鹏','晋鹏','敬曦','景明','景天','景浩','俊晖','君昊','昆琦','昆鹏','昆纬','昆宇','昆锐','昆卉','昆峰','昆颉','昆谊','昆皓','昆鹏','昆明','昆杰','昆雄','昆纶','鹏涛','鹏煊','曦晨','曦之','新曦','旭彬','旭尧','旭鹏','旭东','旭炎','炫明','宣朗','学智','轩昂','彦昌','曜坤','曜栋','曜文','曜曦','曜灿','曜瑞','智伟','智杰','智刚','智阳','昌勋','昌盛','昌茂','昌黎','昌燎','昌翰','晨朗','昂然','昂雄','昂杰','昂熙','范明','飞昂','高朗','高旻','德明','德昌','德曜','智伟','智杰','智刚','智阳','瀚彭','旭炎','宣朗','学智','昊然','昊天','昊苍','昊英','昊宇','昊嘉','昊明','昊伟','鸿朗','华晖','金鹏','晋鹏','敬曦','景明','景天','景浩','景行','景中','景逸','景彰','昆鹏','昆明','昆杰','昆雄','昆纶','鹏涛','鹏煊','景平','俊晖','君昊','昆琦','昆鹏','昆纬','昆宇','昆锐','昆卉','昆峰','昆颉','昆谊','轩昂','彦昌','曜坤','曜文','曜曦','曜灿','曜瑞','曦晨','曦之','新曦','鑫鹏','旭彬','旭尧','旭鹏','旭东','浩轩','浩瀚','浩慨','浩阔','鸿熙','鸿羲','鸿禧','鸿信','泽洋','泽雨','哲瀚','胤运','佑运','允晨','运恒','运发','云天','耘志','耘涛','振荣','振翱','中震','子辰','晗昱','瀚玥','瀚昂','瀚彭','景行','景中','景逸','景彰','绍晖','文景','曦哲','永昌','子昂','智宇','智晖','晗日','晗昱','瀚昂','昊硕','昊磊','昊东','鸿晖','绍晖','文昂','文景','曦哲','永昌','子昂','智宇','智晖','浩然','鸿运','辰龙','运珹','振宇','高朗','景平','鑫鹏','昌淼','炫明','昆皓','曜栋','文昂','恨桃','依秋','依波','香巧','紫萱','涵易','忆之','幻巧','美倩','安寒','白亦','惜玉','碧春','怜雪','听南','念蕾','紫夏','凌旋','芷梦','凌寒','梦竹','千凡','丹蓉','慧贞','思菱','平卉','笑柳','雪卉','南蓉','谷梦','巧兰','绿蝶','飞荷','佳蕊','芷荷','怀瑶','慕易','若芹','紫安','曼冬','寻巧','雅昕','尔槐','以旋','初夏','依丝','怜南','傲菡','谷蕊','笑槐','飞兰','笑卉','迎荷','佳音','梦君','妙绿','觅雪','寒安','沛凝','白容','乐蓉','映安','依云','映冬','凡雁','梦秋','梦凡','秋巧','若云','元容','怀蕾','灵寒','天薇','翠安','乐琴','宛南','怀蕊','白风','访波','亦凝','易绿','夜南','曼凡','亦巧','青易','冰真','白萱','友安','海之','小蕊','又琴','天风','若松','盼菡','秋荷','香彤','语梦','惜蕊','迎彤','沛白','雁彬','易蓉','雪晴','诗珊','春冬','晴钰','冰绿','半梅','笑容','沛凝','映秋','盼烟','晓凡','涵雁','问凝','冬萱','晓山','雁蓉','梦蕊','山菡','南莲','飞双','凝丝','思萱','怀梦','雨梅','冷霜','向松','迎丝','迎梅','雅彤','香薇','以山','碧萱','寒云','向南','书雁','怀薇','思菱','忆文','翠巧','书文','若山','向秋','凡白','绮烟','从蕾','天曼','又亦','从语','绮彤','之玉','凡梅','依琴','沛槐','又槐','元绿','安珊','夏之','易槐','宛亦','白翠','丹云','问寒','易文','傲易','青旋','思真','雨珍','幻丝','代梅','盼曼','妙之','半双','若翠','初兰','惜萍','初之','宛丝','寄南','小萍','静珊','千风','天蓉','雅青','寄文','涵菱','香波','青亦','元菱','翠彤','春海','惜珊','向薇','冬灵','惜芹','凌青','谷芹','雁桃','映雁','书兰','盼香','梅致','寄风','芳荷','绮晴','映之','醉波','幻莲','晓昕','傲柔','寄容','以珊','紫雪','芷容','书琴','美伊','涵阳','怀寒','易云','代秋','惜梦','宇涵','谷槐','怀莲','英莲','芷卉','向彤','新巧','语海','灵珊','凝丹','小蕾','迎夏','慕卉','飞珍','冰夏','亦竹','飞莲','秋月','元蝶','春蕾','怀绿','尔容','小玉','幼南','凡梦','碧菡','初晴','宛秋','傲旋','新之','凡儿','夏真','静枫','芝萱','恨蕊','乐双','念薇','靖雁','菊颂','丹蝶','元瑶','冰蝶','念波','迎翠','海瑶','乐萱','凌兰','曼岚','若枫','傲薇','雅芝','乐蕊','秋灵','凤娇','觅云','依伊','恨山','从寒','忆香','香菱','静曼','青寒','笑天','涵蕾','元柏','代萱','紫真','千青','雪珍','寄琴','绿蕊','荷柳','诗翠','念瑶','兰楠','曼彤','怀曼','香巧','采蓝','芷天','尔曼','巧蕊'];
+        $family = mb_substr($familyName, rand(0, mb_strlen($familyName)-1), 1);
+        $second = $secondName[rand(0, count($secondName))];
+        return $family.$second;
+    }
+
 
 }
-
-/**导出表格
- * @param $filename 导出的csv文件名称
- * @param array $tileArray 所有列名称
- * @param array $dataArray 所有列数据
- */
-/*public function exportToExcel($filename, $tileArray=[], $dataArray=[]){
-    ini_set('memory_limit','512M');
-    ini_set('max_execution_time',0);
-    ob_end_clean();
-    ob_start();
-    header("Content-Type: text/csv");
-    header("Content-Disposition:filename=".$filename);
-    $fp=fopen('php://output','w');
-    fwrite($fp, chr(0xEF).chr(0xBB).chr(0xBF));//转码 防止乱码(比如微信昵称(乱七八糟的))
-    fputcsv($fp,$tileArray);
-    $index = 0;
-    foreach ($dataArray as $item) {
-        if($index==1000){
-            $index=0;
-            ob_flush();
-            flush();
-        }
-        $index++;
-        fputcsv($fp,$item);
-    }
-    ob_flush();
-    flush();
-    ob_end_clean();
-}*/
-
-
-/**导出表格
- * @param $data 一个二维数组,结构如同从数据库查出来的数组
- * @param $title excel的第一行标题,一个数组,如果为空则没有标题
- * @param $filename 导出的xls文件名称
- */
-/*public function exportexcel($dataArray = array(), $titleArray = array(), $filename = 'report') {
-    header ( "Content-type:application/octet-stream" );
-    header ( "Accept-Ranges:bytes" );
-    header ( "Content-type:application/vnd.ms-excel" );
-    header ( "Content-Disposition:attachment;filename=" . $filename . ".xls" );
-    header ( "Pragma: no-cache" );
-    header ( "Expires: 0" );
-    // 导出xls 开始
-    if (! empty ( $title )) {
-        foreach ( $title as $k => $v ) {
-            $title [$k] = iconv ( "UTF-8", "GB2312", $v );
-        }
-        $title = implode ( "\t", $title );
-        echo "$title\n";
-    }
-    if (! empty ( $data )) {
-        foreach ( $data as $key => $val ) {
-            foreach ( $val as $ck => $cv ) {
-                $data [$key] [$ck] = iconv ( "UTF-8", "GB2312", $cv );
-            }
-            $data [$key] = implode ( "\t", $data [$key] );
-        }
-        echo implode ( "\n", $data );
-    }
-}
-
-$ssssd = [1=>'正常 未兑换',2=>'正常 已兑换',3=>'正常 过期',4=>'正常商家已提现',5=>'作废'];
-$count = $res->alias('l')->where($where)->count();// 查询满足要求的总记录数
-$Page  = new \Think\Page($count,10000);// 实例化分页类 传入总记录数和每页显示的记录数(此处调用thinkphp 3.2的分页类)
-$ppp   = ceil($count/10000);
-$pp    = range(1,$ppp);
-foreach ($pp as $kkk => $vvv) {
-    $rs[$kkk] = $res->field('l.id lid,l.user_id,l.shop_id,l.code_num,l.code_src,l.winning_time,l.exchange_time,l.status,l.item_id iid,i.title,i.money,s.coder,u.nickname,u.head,s.manager,s.phone')->alias('l')->where($where)->join('sd_item i ON i.id=l.item_id')->join('left join  sd_shop s ON s.id=l.shop_id')->join('sd_user u ON u.id=l.user_id')->page($vvv.', 10000')->select();
-    $str[$kkk] = "用户昵称,兑奖门店号,核销员,核销员手机号,中奖时间,兑换时间,奖项名称,中奖金额,中奖码,兑换状态";
-    $exl11[$kkk] = explode(',',$str[$kkk]);
-    foreach ($rs[$kkk] as $k => $v){
-        if (!$v['nickname']) $v['nickname']           = '暂无数据';
-        if (!$v['coder']) $v['coder']                 = '暂无数据';
-        if (!$v['manager']) $v['manager']             = '暂无数据';
-        if (!$v['phone']) $v['phone']                 = '暂无数据';
-        if (!$v['winning_time']) $v['winning_time']   = '暂无数据';
-        if (!$v['exchange_time']) $v['exchange_time'] = '暂无数据';
-        if (!$v['title']) $v['title']                 = '暂无数据';
-        if (!$v['money']) $v['money']                 = '暂无数据';
-       
-        $exl[$kkk][] = array(
-            $v['nickname'],$v['coder'],$v['manager'],$v['phone'],$v['winning_time'],$v['exchange_time'],$v['title'],$v['money'],$ssssd[$v['status']]
-        );
-    }
-
-    $this->exportToExcel('兑奖记录_'.date('Ymd').'.csv', $exl11[$kkk], $exl[$kkk]);//cvs
-    $this->exportexcel($exl[$kkk], $exl11[$kkk], '兑奖记录_'.date('Ymd').'.xls');//xls
-}*/
-
 
 
